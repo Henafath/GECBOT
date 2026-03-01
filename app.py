42100d4d929f60ffac04045c4491c645943996ed
hena fathima m <henafathima24@gmail.com>
2026-03-01 18:19:44 +0530

    debugging ml model...2

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-from flask import Flask, jsonify,request, render_template
+from flask import Flask
 from routes.chatbot_routes import chatbot_bp
-from routes.department_routes import department_bp, fetch_ug_programs, fetch_pg_programs, fetch_departments, get_department
-from routes.faculty_routes import faculty_bp, df_get_all_faculties, df_get_faculty_contacts, df_get_faculty_by_department
-from routes.placement_routes import placement_bp, df_get_all_placements, df_get_placements_by_year
-from routes.contact_routes import df_get_contact
-from ml.predict import predict_answer
+from routes.department_routes import department_bp
+from routes.faculty_routes import faculty_bp
+from routes.placement_routes import placement_bp
 from routes.admin_routes import admin_bp
-from ml.train_model import train
-from datetime import datetime,timezone
-from services.db_service import get_db
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
